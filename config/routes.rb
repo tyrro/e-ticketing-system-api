90b3d5c5fbfd57253974a884830be9cfec4cb2e2
rajibds <rajib.das.shuva@gmail.com>
2022-01-15 12:42:22 +0600

    chore: Add users signup and login routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,13 @@
 
 Rails.application.routes.draw do
   scope defaults: { format: 'json' } do
+    resources :users, only: [] do
+      collection do
+        post :signup
+        post :login
+      end
+    end
+
     resources :events, only: %i(index show)
 
     resources :tickets, only: %i(index) do
