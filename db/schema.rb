169cc8d76646d85a4e22b762f4a62c4a7eba8961
rajibds <rajib.das.shuva@gmail.com>
2022-01-15 12:39:10 +0600

    feat: Add Users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_09_162021) do
+ActiveRecord::Schema.define(version: 2022_01_14_183501) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "events", force: :cascade do |t|
     t.string "name"
@@ -26,6 +29,13 @@ ActiveRecord::Schema.define(version: 2020_12_09_162021) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["event_id"], name: "index_tickets_on_event_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "tickets", "events"
